1f5bb27d6c6d66be3459c8d3d36a8e01dfa9db08
Evallejos96 <96029483+Evallejos96@users.noreply.github.com>
2025-12-09 15:51:22 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,14 +4,15 @@ let app = express();
 // Ejercicio 1
 console.log("Hello World");
 
-// Ejercicio 2 (comentado)
-// app.get("/", function(req, res) {
-//   res.send("Hello Express");
-// });
+// Ejercicio 2 (comentado si está)
+/// app.get("/", (req, res) => { res.send("Hello Express"); });
 
-// Ejercicio 3: enviar index.html
+// Ejercicio 3 - enviar index.html
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+// Ejercicio 4 - contenido estático
+app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
