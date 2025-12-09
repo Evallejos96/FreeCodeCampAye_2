98835f907d9c5cd98844b89c79b860d4ed1128a5
Evallejos96 <96029483+Evallejos96@users.noreply.github.com>
2025-12-09 15:30:02 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,13 @@
 let express = require('express');
 let app = express();
 
+// ejercicio 1
 console.log("Hello World");
+
+// ejercicio 2
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
 
 module.exports = app;
 
